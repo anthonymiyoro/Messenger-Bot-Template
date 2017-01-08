362ea6e26c9fe87d01c643ada97b899a77543be6
anthonymiyoro <amiyoro@gmail.com>
2017-01-08 22:18:26 +0300

    Added implementtion to main method

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ import gym
 import universe
 import random
 
+def determine_turn(turn, j, total_sum, reward, prev_total_sum, observation_n)
+
 
 def main():
 
@@ -25,5 +27,36 @@ def main():
     right = [('KeyEvent', 'ArrowUp', True) ,('KeyEvent', 'ArrowLeft', False) ,('KeyEvent', 'ArrowRight', True)]
     forward = [('KeyEvent', 'ArrowUp', True) ,('KeyEvent', 'ArrowLeft', False) ,('KeyEvent', 'ArrowRight', False)]
 
-    
+    while True:
+    	# add iterations
+    	n+=1
 
+    	if(n > 1):
+    	# check for first feedback
+	    	if(observation_n[0] != None):
+	    		# put the reward in prev_score
+	    		prev_score = reward_n[0]
+
+	    		# if turn is true, pick a random event
+	    		if(turn):
+	    			event = random.choice([left, right])
+	    			# perform an action
+	    			action_n = [event for ob in observation_n]
+	    			# return turn to false
+	    			turn = False
+
+		# if no turn is needed go forward
+		elif(~turn):
+			action_n = [forward for ob in observation_n]
+
+		# if there is feedback then check if you need to make a turn
+		if(observation_n[0] != None):
+			turn, j, total_sum, prev_total_sum = determine_turn(turn, j, total_sum, reward, prev_total_sum, observation_n[0])
+
+		#save new values after implementation
+		observation_n, reward_n, done_n, info = env.step(action_n)
+
+		env.render()
+
+	if __name__ == '__main__':
+		main()
