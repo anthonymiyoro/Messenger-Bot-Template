16b160ea74639ac14806b69bd7c83589a52ab420
Anthony Miyoro <amiyoro@gmail.com>
2017-10-25 14:30:21 +0300

    REmoved shop bot

--- shop_bot/config.py
+++
@@ -1,5 +0,0 @@
-CONFIG = {
-    'FACEBOOK_TOKEN': 'EAAHUZAIvkVjwBAP2ZA99jI1U357BZBkjQ4UpSZBojNnZB1ZCn1Hl2KR6S93kjWZAzSbuLTwuX1h98kAZAIjs5rYpBcwKxdLF1mFlnqC2GIMSoZAioyW4orexO9tCCHvdCiPv0dmbpMiiDkrbn45WWePx8TkHDYZCnnWEO7sZCoQTIKTAwZDZD',
-    'VERIFY_TOKEN': 'my_verify_token',
-    'SERVER_URL': 'https://amiyoro.pythonanywhere.com'
-}
