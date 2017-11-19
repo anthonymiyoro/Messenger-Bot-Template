9958f73f285cd271a378b0489ae79bf9df84abb2
Anthony Miyoro <amiyoro@gmail.com>
2017-11-19 23:24:11 +0300

    Updated gitignore and config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+CONFIG = {
+     'FACEBOOK_TOKEN': 'EAAHUZAIvkVjwBADigzcYHaNOZCI5lZAzR7LqKdDj1X350qkeytlOYWuTUDGfv86kSeCEyDZAazo9ZClYU7j13ZC9Vg2xHpZCNodvt6ZCGGbA9tWELfQMWZCLZCXAh887iQ6fJuiQCBZAfjOxgG4UYCtCMvhcGAj5bes8lcDm3UtPvBXZCgZDZD',
+     'VERIFY_TOKEN': 'my_verify_token',
+     'SERVER_URL': 'https://shopping-example-bot.herokuapp.com/'
+ }
