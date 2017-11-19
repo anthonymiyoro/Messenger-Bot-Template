5428907f6c03d24aab02b2c3f6e19e297454a31b
Anthony Miyoro <amiyoro@gmail.com>
2017-11-19 23:30:03 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-     'FACEBOOK_TOKEN': 'EAAHUZAIvkVjwBADigzcYHaNOZCI5lZAzR7LqKdDj1X350qkeytlOYWuTUDGfv86kSeCEyDZAazo9ZClYU7j13ZC9Vg2xHpZCNodvt6ZCGGbA9tWELfQMWZCLZCXAh887iQ6fJuiQCBZAfjOxgG4UYCtCMvhcGAj5bes8lcDm3UtPvBXZCgZDZD',
+     'FACEBOOK_TOKEN': 'FACEBOOK_TOKEN_HERE',
      'VERIFY_TOKEN': 'my_verify_token',
-     'SERVER_URL': 'https://shopping-example-bot.herokuapp.com/'
+     'SERVER_URL': 'SERVER_URL_HERE'
  }
